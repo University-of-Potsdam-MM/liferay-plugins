3ce7d7409c8a35f24949b1396b5c0c0701d4b72d
Maren <maren.schulze@uni-potsdam.de>
2015-11-05 11:06:24 +0100

    desktopansicht iframe theme

---
+++ themes/so-up-iframe-theme/docroot/_diffs/js/custom.js
@@ -0,0 +1,51 @@
+/* toggle worskpaces navigation dropdown */
+
+$('document').ready(clickdropdown); 
+
+
+		
+function clickdropdown() {			
+	
+			$('.click-nav ul').toggleClass('no-js js');
+			$('.scope-mobile').toggleClass('no-js js');
+			$('.click-nav .js').hide();
+			$('.scope-mobile .js').hide();
+			$('.clicker').click(function(e) {
+				$('.click-nav .js').slideToggle(200);
+				$('.scope-mobile .js').slideToggle(200);
+				$('.clicker').toggleClass('active');
+				
+				e.stopPropagation();
+			});
+			$(document).click(function() {
+				if ($('.click-nav .js ul').is(':visible')) {
+					$('.click-nav .js ul', this).slideUp();
+					$('.scope-mobile .js', this).slideUp();
+					$('.clicker').removeClass('active');
+				}
+			});
+}
+/*
+$('document').ready(function($) {
+
+    function sticky()
+    {
+        var window_top=$(window).scrollTop();
+        var top_position=$('body').offset().top;
+        var element_to_stick=$('.container-fluid');
+        var winwidth=$(window).width();
+        
+        if (window_top > top_position && winwidth>978) {
+            element_to_stick.addClass('smallhead');
+          
+        } else {
+            element_to_stick.removeClass('smallhead');
+        }
+    }
+    sticky();
+    
+    $(window).scroll(function() {
+        sticky();
+    });
+    
+});*/
