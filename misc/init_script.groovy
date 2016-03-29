c36db0f24d85ea8dd8d3e543fadfa2d36d3018ca
Matthias Weise <matweise@uni-potsdam.de>
2016-03-29 12:13:22 +0200

    layoutsUpdateable is now set to true. Otherwise they user created layouts have no CUSTOMIZE permission

--- misc/init_script.groovy
+++ misc/init_script.groovy
@@ -18,46 +18,47 @@ String USER_HOME_LAYOUT_SET_PROTOTYPE_NAME = "Social Office User Home";
 String USER_PROFILE_LAYOUT_SET_PROTOTYPE_NAME = "Social Office User Profile";
 
 
-// Make pages in users personal area uneditable
+// Make predefined pages in users personal area uneditable
 setLayoutSetPrototypeUneditable(USER_HOME_LAYOUT_SET_PROTOTYPE_NAME);
 
-// Make pages in users profile uneditable
+// Make predefined pages in users profile uneditable
 setLayoutSetPrototypeUneditable(USER_PROFILE_LAYOUT_SET_PROTOTYPE_NAME);
 
 
 /**
-* Makes LayoutSets inheriting from the given LayoutSetPrototype and the
-* corresponding Layouts uneditable by the user
-* 
-* @param layoutSetPrototypeName
-*            Name of the LayoutSetPrototype
-*/
+ * Makes the pages of LayoutSets inheriting from the given
+ * LayoutSetPrototype uneditable by the user. NOTE: layoutUpdateable needs
+ * to be set to true. Otherwise pages will loose the permission CUSTOMIZE
+ * (important for the custom-pages-portlet).
+ * 
+ * @param layoutSetPrototypeName
+ *            Name of the LayoutSetPrototype
+ */
 void setLayoutSetPrototypeUneditable(String layoutSetPrototypeName) throws Exception {
-  List<LayoutSetPrototype> layoutSetPrototypes = LayoutSetPrototypeLocalServiceUtil
-  .getLayoutSetPrototypes(PortalUtil.getDefaultCompanyId());
-  
-  // Make pages in users personal area not editable
-  for (LayoutSetPrototype layoutSetPrototype : layoutSetPrototypes) {
-    if (layoutSetPrototype.getName(Locale.ENGLISH).equals(layoutSetPrototypeName)) {
-      out.println("Found User Home LayoutSetPrototype " + layoutSetPrototypeName + ". layoutSetPrototypeId: "
-                  + layoutSetPrototype.getLayoutSetPrototypeId());
-      
-      UnicodeProperties properties = layoutSetPrototype.getSettingsProperties();
-      properties.setProperty("layoutsUpdateable", "false");
-      layoutSetPrototype.setSettingsProperties(properties);
-      LayoutSetPrototypeLocalServiceUtil.updateLayoutSetPrototype(layoutSetPrototype);
-      out.println("Set property layoutsUpdateable of the LayoutSetPrototype " + layoutSetPrototypeName
-                  + " to false");
-      
-      List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(layoutSetPrototype.getGroupId(), true);
-      for (Layout layout : layouts) {
-        layout.getLayoutType().setTypeSettingsProperty("layoutUpdateable", "false");
-        LayoutLocalServiceUtil.updateLayout(layout);
-        out.println("Set property layoutUpdateable of the layout " + layout.getName(Locale.ENGLISH)
-                    + " to false");
-      }
-      out.println();
-      break;
-    }
-  }
+	List<LayoutSetPrototype> layoutSetPrototypes = LayoutSetPrototypeLocalServiceUtil
+			.getLayoutSetPrototypes(PortalUtil.getDefaultCompanyId());
+
+	for (LayoutSetPrototype layoutSetPrototype : layoutSetPrototypes) {
+		if (layoutSetPrototype.getName(Locale.ENGLISH).equals(layoutSetPrototypeName)) {
+			out.println("Found User Home LayoutSetPrototype " + layoutSetPrototypeName + ". layoutSetPrototypeId: "
+					+ layoutSetPrototype.getLayoutSetPrototypeId());
+
+			UnicodeProperties properties = layoutSetPrototype.getSettingsProperties();
+			properties.setProperty("layoutsUpdateable", "true");
+			layoutSetPrototype.setSettingsProperties(properties);
+			LayoutSetPrototypeLocalServiceUtil.updateLayoutSetPrototype(layoutSetPrototype);
+			out.println("Set property layoutsUpdateable of the LayoutSetPrototype " + layoutSetPrototypeName
+					+ " to true");
+
+			List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(layoutSetPrototype.getGroupId(), true);
+			for (Layout layout : layouts) {
+				layout.getLayoutType().setTypeSettingsProperty("layoutUpdateable", "false");
+				LayoutLocalServiceUtil.updateLayout(layout);
+				out.println("Set property layoutUpdateable of the layout " + layout.getName(Locale.ENGLISH)
+						+ " to false");
+			}
+			out.println();
+			break;
+		}
+	}
 }
