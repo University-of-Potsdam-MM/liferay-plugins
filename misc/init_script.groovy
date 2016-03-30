9517cf0a4c5fa2b8eb3f12881416cc1ccfeaa7f5
Matthias Weise <matweise@uni-potsdam.de>
2016-03-30 11:11:15 +0200

    Updated init script - descriptions of the layout templates will now be set

--- misc/init_script.groovy
+++ misc/init_script.groovy
@@ -4,20 +4,36 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.events.Action;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutPrototype;
 import com.liferay.portal.model.LayoutSetPrototype;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.LayoutPrototypeLocalServiceUtil;
 import com.liferay.portal.service.LayoutSetPrototypeLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
 
 
 String USER_HOME_LAYOUT_SET_PROTOTYPE_NAME = "Social Office User Home";
 String USER_PROFILE_LAYOUT_SET_PROTOTYPE_NAME = "Social Office User Profile";
-String GROUP_WORK_WORKSPACE_NAME = "Group Work";
-String GROUP_WORK_DESCRIPTION = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.";
+
+String GROUP_WORK_WORKSPACE_TEMPLATE_NAME = "Group Work";
+String GROUP_WORK_WORKSPACE_TEMPLATE_DESCRIPTION = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.";
+
+String BLOG_PAGE_TEMPLATE_NAME = "Page with Blog";
+String BLOG_PAGE_TEMPLATE_DESCRIPTION = "Diese Seite beinhaltet einen Blog, der z.B. als Lerntagebuch oder für Berichte genutzt werden kann. Es können weitere Anwendungen hinzugefügt werden.";
+
+String WIKI_PAGE_TEMPLATE_NAME = "Page with Wiki";
+String WIKI_PAGE_TEMPLATE_DESCRIPTION = "Diese Seite beinhaltet ein Wiki, das z.B. als Glossar oder für Dokumentationen genutzt werden kann. Es können weitere Anwendungen hinzugefügt werden.";
+
+String WEB_CONTENT_PAGE_TEMPLATE_NAME = "Page with Text-Image Editor";
+String WEB_CONTENT_PAGE_TEMPLATE_DESCRIPTION = "Diese Seite beinhaltet einen Editor, der z.B. für Texte mit Grafiken und Weblinks genutzt werden kann. Es können weitere Anwendungen hinzugefügt werden.";
+
+String PAGE_WITHOUT_APPLICATIONS_TEMPLATE_NAME = "Page without Applications";
+String PAGE_WITHOUT_APPLICATIONS_TEMPLATE_DESCRIPTION = "Diese Seite ist leer und kann nach dem Erstellen beliebig mit Anwendungen und Inhalten bestückt werden.";
 
 try {
 
@@ -28,7 +44,21 @@ try {
 	setLayoutSetPrototypeUneditable(USER_PROFILE_LAYOUT_SET_PROTOTYPE_NAME);
 
 	// Set description of the group work workspace
-	setLayoutSetPrototypeDescription(GROUP_WORK_WORKSPACE_NAME, GROUP_WORK_DESCRIPTION);
+	setLayoutSetPrototypeDescription(GROUP_WORK_WORKSPACE_TEMPLATE_NAME,
+			GROUP_WORK_WORKSPACE_TEMPLATE_DESCRIPTION);
+
+	// Set description of the blog page layout prototype
+	setLayoutPrototypeDescription(BLOG_PAGE_TEMPLATE_NAME, BLOG_PAGE_TEMPLATE_DESCRIPTION);
+
+	// Set description of the wiki page layout prototype
+	setLayoutPrototypeDescription(WIKI_PAGE_TEMPLATE_NAME, WIKI_PAGE_TEMPLATE_DESCRIPTION);
+
+	// Set description of the web content page layout prototype
+	setLayoutPrototypeDescription(WEB_CONTENT_PAGE_TEMPLATE_NAME, WEB_CONTENT_PAGE_TEMPLATE_DESCRIPTION);
+
+	// Set description of the page without applications layout prototype
+	setLayoutPrototypeDescription(PAGE_WITHOUT_APPLICATIONS_TEMPLATE_NAME,
+			PAGE_WITHOUT_APPLICATIONS_TEMPLATE_DESCRIPTION);
 
 } catch (Exception e) {
 	e.printStackTrace();
@@ -43,6 +73,7 @@ try {
  * 
  * @param layoutSetPrototypeName
  *            Name of the LayoutSetPrototype
+ * @throws Exception
  */
 void setLayoutSetPrototypeUneditable(String layoutSetPrototypeName) throws Exception {
 	List<LayoutSetPrototype> layoutSetPrototypes = LayoutSetPrototypeLocalServiceUtil
@@ -73,6 +104,7 @@ void setLayoutSetPrototypeUneditable(String layoutSetPrototypeName) throws Excep
 		}
 	}
 	out.println("WARNING: could not find LayoutSetPrototype " + layoutSetPrototypeName);
+	out.println();
 }
 
 /**
@@ -100,8 +132,42 @@ void setLayoutSetPrototypeDescription(String layoutSetPrototypeName, String layo
 			out.println("Set description of the LayoutSetPrototype " + layoutSetPrototypeName + " to: "
 					+ layoutSetPrototypeDescription);
 
+			out.println();
 			return;
 		}
 	}
 	out.println("WARNING: could not find LayoutSetPrototype " + layoutSetPrototypeName);
+	out.println();
+}
+
+/**
+ * Sets the description of a LayoutPrototype
+ * 
+ * @param layoutPrototypeName
+ *            Name of the LayoutProtoype
+ * @param layoutPrototypeDescription
+ *            Description of the LayoutPrototype
+ * @throws Exception
+ */
+void setLayoutPrototypeDescription(String layoutPrototypeName, String layoutPrototypeDescription) throws Exception {
+	List<LayoutPrototype> layoutPrototypes = LayoutPrototypeLocalServiceUtil.getLayoutPrototypes(QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS);
+
+	out.println("Try to set description of the LayoutPrototype " + layoutPrototypeName);
+	for (LayoutPrototype layoutPrototype : layoutPrototypes) {
+		if (layoutPrototype.getName(Locale.ENGLISH).equals(layoutPrototypeName)) {
+			out.println("Found LayoutPrototype " + layoutPrototypeName + ". layoutPrototypeId: "
+					+ layoutPrototype.getLayoutPrototypeId());
+
+			layoutPrototype.setDescription(layoutPrototypeDescription);
+			LayoutPrototypeLocalServiceUtil.updateLayoutPrototype(layoutPrototype);
+			out.println("Set description of the LayoutPrototype " + layoutPrototypeName + " to: "
+					+ layoutPrototypeDescription);
+
+			out.println();
+			return;
+		}
+	}
+	out.println("WARNING: could not find LayoutPrototype " + layoutPrototypeName);
+	out.println();
 }
