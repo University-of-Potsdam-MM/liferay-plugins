97960ac35d125b0285056381156d8b8adaf780b4
Matthias Weise <matweise@uni-potsdam.de>
2016-11-08 10:21:55 +0100

    Solves bug where the user created sites are not listed correctly. Refs #658

--- portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/sites/util/SitesUtil.java
+++ portlets/so-portlet/docroot/WEB-INF/src/com/liferay/so/sites/util/SitesUtil.java
@@ -17,6 +17,9 @@
 
 package com.liferay.so.sites.util;
 
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.Group;
@@ -36,100 +39,101 @@ import java.util.List;
  */
 public class SitesUtil {
 
-	public static List<Group> getFavoriteSitesGroups(
-			long userId, String name, int start, int end)
-		throws Exception {
+	public static List<Group> getFavoriteSitesGroups(long userId, String name, int start, int end) throws Exception {
 
-		List<Object[]> favoriteSites =
-			FavoriteSiteLocalServiceUtil.getFavoriteSites(
-				userId, name, start, end);
+		List<Object[]> favoriteSites = FavoriteSiteLocalServiceUtil.getFavoriteSites(userId, name, start, end);
 
 		List<Group> groups = new ArrayList<Group>(favoriteSites.size());
 
 		for (Object[] favoriteSite : favoriteSites) {
-			long curUserId = (Long)favoriteSite[0];
-			long groupId = (Long)favoriteSite[1];
+			long curUserId = (Long) favoriteSite[0];
+			long groupId = (Long) favoriteSite[1];
 
 			try {
 				groups.add(GroupLocalServiceUtil.getGroup(groupId));
-			}
-			catch (Exception e) {
-				FavoriteSiteLocalServiceUtil.deleteFavoriteSites(
-					curUserId, groupId);
+			} catch (Exception e) {
+				FavoriteSiteLocalServiceUtil.deleteFavoriteSites(curUserId, groupId);
 			}
 		}
 
 		return groups;
 	}
 
-	public static int getFavoriteSitesGroupsCount(long userId, String name)
-		throws Exception {
+	public static int getFavoriteSitesGroupsCount(long userId, String name) throws Exception {
 
 		return FavoriteSiteLocalServiceUtil.getFavoriteSitesCount(userId, name);
 	}
 
-	public static List<Group> getVisibleSites(
-		long companyId, long userId, String keywords, FilterType filterType,
-		int start, int end) {
+	public static List<Group> getVisibleSites(long companyId, long userId, String keywords, FilterType filterType,
+			int start, int end) {
 
 		try {
-			return doGetVisibleSites(
-				companyId, userId, keywords, filterType, start, end);
-		}
-		catch (Exception e) {
+			return doGetVisibleSites(companyId, userId, keywords, filterType, start, end);
+		} catch (Exception e) {
 			_log.error(e, e);
 
 			return new ArrayList<Group>(0);
 		}
 	}
 
-	public static int getVisibleSitesCount(
-		long companyId, long userId, String keywords, FilterType filterType) {
+	public static int getVisibleSitesCount(long companyId, long userId, String keywords, FilterType filterType) {
 
 		try {
-			return doGetVisibleSitesCount(
-				companyId, userId, keywords, filterType);
-		}
-		catch (Exception e) {
+			return doGetVisibleSitesCount(companyId, userId, keywords, filterType);
+		} catch (Exception e) {
 			_log.error(e, e);
 
 			return 0;
 		}
 	}
 
-	protected static List<Group> doGetVisibleSites(
-			long companyId, long userId, String keywords, FilterType filterType,
-			int start, int end)
-		throws Exception {
+	protected static List<Group> doGetVisibleSites(long companyId, long userId, String keywords, FilterType filterType,
+			int start, int end) throws Exception {
 
 		keywords = CustomSQLUtil.keywords(keywords)[0];
 
-		if (filterType == FilterType.SITE_OWNER || filterType == FilterType.USERS_SITES) {
-			LinkedHashMap<String, Object> params =
-				new LinkedHashMap<String, Object>();
+		// BEGIN CHANGE
+		// Solve bug where not all sites are visible (#658)
+		// if (filterType == FilterType.SITE_OWNER || filterType ==
+		// FilterType.USERS_SITES) {
+		if (filterType == FilterType.USERS_SITES) {
+			// END CHANGE
+			LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
 
 			params.put("active", Boolean.TRUE);
 			params.put("pageCount", Boolean.TRUE);
 			params.put("usersGroups", userId);
-			
-			List<Group> groups = GroupLocalServiceUtil.search(
-				companyId, keywords, null, params, true, start, end,
-				new GroupNameComparator(true));
-			
-			if (filterType == FilterType.SITE_OWNER){
+
+			List<Group> groups = GroupLocalServiceUtil.search(companyId, keywords, null, params, true, start, end,
+					new GroupNameComparator(true));
+
+			// BEGIN CHANGE
+			// if reduces actual count leading to inconsistencies on the
+			// following pages
+			/* if (filterType == FilterType.SITE_OWNER) {
 				List<Group> filteredGroups = new ArrayList<Group>();
-				for (Group group : groups){
+				for (Group group : groups) {
 					if (group.getCreatorUserId() == userId)
 						filteredGroups.add(group);
 				}
 				return filteredGroups;
-			}
+			} */
+			// END CHANGE
 
 			return groups;
 		}
+		// BEGIN CHANGE
+		// Do request without reducing the site count afterwards
+		else if (filterType == FilterType.SITE_OWNER){
+			DynamicQuery query = DynamicQueryFactoryUtil.forClass(Group.class)
+					.add(PropertyFactoryUtil.forName("creatorUserId").eq(new Long(userId)))
+					.add(PropertyFactoryUtil.forName("active").eq(new Boolean(true)))
+					.add(PropertyFactoryUtil.forName("site").eq(new Boolean(true)));
+			return GroupLocalServiceUtil.dynamicQuery(query,start,end);
+		}
+		// END CHANGE
 
-		LinkedHashMap<String, Object> params =
-			new LinkedHashMap<String, Object>();
+		LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
 
 		params.put("active", Boolean.TRUE);
 		params.put("pageCount", Boolean.TRUE);
@@ -141,45 +145,46 @@ public class SitesUtil {
 
 		params.put("types", types);
 
-		List<Group> groups = GroupLocalServiceUtil.search(
-			companyId, keywords, null, params, true, start, end,
-			new GroupNameComparator(true));
+		List<Group> groups = GroupLocalServiceUtil.search(companyId, keywords, null, params, true, start, end,
+				new GroupNameComparator(true));
 
 		return groups;
 	}
 
-	protected static int doGetVisibleSitesCount(
-			long companyId, long userId, String keywords, FilterType filterType)
-		throws Exception {
+	protected static int doGetVisibleSitesCount(long companyId, long userId, String keywords, FilterType filterType)
+			throws Exception {
 
 		keywords = CustomSQLUtil.keywords(keywords)[0];
 
 		if (filterType == FilterType.SITE_OWNER || filterType == FilterType.USERS_SITES) {
-			LinkedHashMap<String, Object> params =
-				new LinkedHashMap<String, Object>();
+			LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
 
 			params.put("active", Boolean.TRUE);
 			params.put("pageCount", Boolean.TRUE);
 			params.put("usersGroups", userId);
-			
-			if (filterType == FilterType.SITE_OWNER){
-				List<Group> groups = GroupLocalServiceUtil.search(
-						companyId, keywords, null, params, true, -1, -1,
+
+			if (filterType == FilterType.SITE_OWNER) {
+				// BEGIN CHANGE
+				// correct request according to the change in the doGetVisibleSites method
+				/* List<Group> groups = GroupLocalServiceUtil.search(companyId, keywords, null, params, true, -1, -1,
 						new GroupNameComparator(true));
 				List<Group> filteredGroups = new ArrayList<Group>();
-				for (Group group : groups){
+				for (Group group : groups) {
 					if (group.getCreatorUserId() == userId)
 						filteredGroups.add(group);
 				}
-				return filteredGroups.size();
+				return filteredGroups.size(); */
+				DynamicQuery query = DynamicQueryFactoryUtil.forClass(Group.class)
+						.add(PropertyFactoryUtil.forName("creatorUserId").eq(new Long(userId)))
+						.add(PropertyFactoryUtil.forName("active").eq(new Boolean(true)))
+						.add(PropertyFactoryUtil.forName("site").eq(new Boolean(true)));
+				return (int) GroupLocalServiceUtil.dynamicQueryCount(query);
+				// END CHANGE
 			}
 
-			return GroupLocalServiceUtil.searchCount(
-				companyId, keywords, null, params, true);
-		}
-		else {
-			LinkedHashMap<String, Object> params =
-				new LinkedHashMap<String, Object>();
+			return GroupLocalServiceUtil.searchCount(companyId, keywords, null, params, true);
+		} else {
+			LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
 
 			params.put("active", Boolean.TRUE);
 			params.put("pageCount", Boolean.TRUE);
@@ -191,13 +196,12 @@ public class SitesUtil {
 
 			params.put("types", types);
 
-			return GroupLocalServiceUtil.searchCount(
-				companyId, keywords, null, params, true);
+			return GroupLocalServiceUtil.searchCount(companyId, keywords, null, params, true);
 		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(SitesUtil.class);
-	
+
 	public enum FilterType {
 		ALL_SITES, USERS_SITES, SITE_OWNER
 	}
