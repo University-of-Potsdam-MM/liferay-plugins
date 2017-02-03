59e39ccf9495730293e4f79d13bd1bc63ff19dba
wolfien <wolfien@uni-potsdam.de>
2017-02-03 14:15:58 +0100

    added ActivitiesUserListiner, to add user to moodle if new campus.up user is created refs #196

---
+++ portlets/up-activities-portlet/docroot/WEB-INF/src/de/unipotsdam/elis/activities/model/listeners/ActivitiesUserListener.java
@@ -0,0 +1,26 @@
+package de.unipotsdam.elis.activities.model.listeners;
+
+import javax.ws.rs.ClientErrorException;
+
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.model.BaseModelListener;
+import com.liferay.portal.model.User;
+
+import de.unipotsdam.elis.activities.extservice.moodle.rest.MoodleRestClient;
+
+public class ActivitiesUserListener extends BaseModelListener<User> {
+
+	@Override
+	public void onAfterCreate(User user) throws ModelListenerException {
+
+		try {
+			// TODO do not add default user, can default user be created?
+			MoodleRestClient.addCampusUpUser(user.getScreenName());
+		} catch (JSONException | ClientErrorException e) {
+			throw new ModelListenerException(e);
+		}
+
+	}
+
+}
