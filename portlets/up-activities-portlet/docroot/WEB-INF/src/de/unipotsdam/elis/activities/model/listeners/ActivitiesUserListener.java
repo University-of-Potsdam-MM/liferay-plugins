941bb1d3ae6c258fcf11b078eb67d43d7d0a5ac9
wolfien <wolfien@uni-potsdam.de>
2017-03-23 12:19:59 +0100

    fixed problem that prevents users from being created if moodle is not available

--- portlets/up-activities-portlet/docroot/WEB-INF/src/de/unipotsdam/elis/activities/model/listeners/ActivitiesUserListener.java
+++ portlets/up-activities-portlet/docroot/WEB-INF/src/de/unipotsdam/elis/activities/model/listeners/ActivitiesUserListener.java
@@ -1,9 +1,6 @@
 package de.unipotsdam.elis.activities.model.listeners;
 
-import javax.ws.rs.ClientErrorException;
-
 import com.liferay.portal.ModelListenerException;
-import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.model.BaseModelListener;
 import com.liferay.portal.model.User;
 
@@ -15,10 +12,9 @@ public class ActivitiesUserListener extends BaseModelListener<User> {
 	public void onAfterCreate(User user) throws ModelListenerException {
 
 		try {
-			// TODO do not add default user, can default user be created?
 			MoodleRestClient.addCampusUpUser(user.getScreenName());
 		} catch (Exception e) {
-			throw new ModelListenerException(e);
+			// just do nothing exception prevents users form being created
 		}
 
 	}
